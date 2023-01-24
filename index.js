21f50607aa045f98c9a3db0ac12e7b66e064eb4e
Patrick Simmons <hadroncollides@gmail.com>
2023-01-23 20:29:53 -0800

    MVP

--- index.js
+++ index.js
@@ -64,10 +64,13 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random() * Math.floor(3));
 }
-
+console.log(inning)
+console.log(inning)
+console.log(inning)
+console.log(inning)
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -83,8 +86,19 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*Code Here*/){
-  /*Code Here*/
+function finalScore(inningCB, innings){
+  let homeScore = 0;
+  let awayScore = 0;
+  for (let i = 0; i < innings; i++){
+    homeScore = homeScore + inningCB();
+    awayScore = homeScore + inningCB();
+  }
+  return {
+    Home: homeScore,
+    Away: awayScore
+  }
+  
+  
 }
 
 
@@ -101,8 +115,11 @@ For example: invoking getInningScore(inning) might return this object:
   */
 
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB, ) {
+  return {
+    Home: inningCB(), 
+    Away: inningCB()
+  }
 
 }
 
